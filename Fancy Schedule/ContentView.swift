558d196cfcfb43a76091d00cbf5b60a19ddcffc8
Krystian Postek <krystian.postek@ledatel.pl>
2022-06-27 22:13:44 +0200

    Initial Commit

---
+++ Fancy Schedule/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Fancy Schedule
+//
+//  Created by Krystian Postek on 27/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fancy Schedule/Fancy_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fancy_ScheduleApp.swift
+//  Fancy Schedule
+//
+//  Created by Krystian Postek on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fancy_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
